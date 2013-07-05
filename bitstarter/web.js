7203b83bc578957b04535dced112b7a4a58de058
Anson Zeall <ubuntu@ip-10-212-115-78.ec2.internal>
2013-07-05 04:49:11 +0000

    removed annoying.text

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  //response.send('hello World 3!');
+  var buffer = new Buffer(16);
+  buffer.write("Hello World!", "utf-8");
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
