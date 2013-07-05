48713efb5c3f0e94fbb5713c2c37dd2b28ae6b2f
Anson Zeall <ubuntu@ip-10-212-115-78.ec2.internal>
2013-07-05 16:53:56 +0000

    changed it all again

--- bitstarter/web.js
+++ bitstarter/web.js
@@ -1,15 +1,11 @@
-
-
 var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-
-  response.send(fs.readFile('./index.html').toString('utf-8'));
+  response.send(fs.readSyncFile('./index.html').toString('utf-8'));
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
